e13532ba0fb9319a509fe885f2bc189723254a3f
rohan-khapane <rohankhapane91827@gmail.com>
2024-11-28 15:46:30 +0530

    added comment in the file for future understanding

--- app/blueprints/file_upload.py
+++ app/blueprints/file_upload.py
@@ -22,23 +22,25 @@ def file():
 
 @bp.route('/csv/',methods=['GET','POST'])
 def csv():
-    file2=request.files['file2']
-    dataframe=pd.read_excel(file2)
-    if not os.path.exists('Downloads'):
+    file2=request.files['file2']#got the file form the html page in using the name tag
+    dataframe=pd.read_excel(file2) #convert it to dataframe 
+    if not os.path.exists('Downloads'):  #creation of the new folder using the os module if not exists
         print('creating downloading folder')
         os.makedirs('Downloads')
-    filename=f'{uuid.uuid4()}.csv'
-    dataframe.to_csv(os.path.join('Downloads',filename))
-    return render_template('download.html',filename=filename)
+    filename=f'{uuid.uuid4()}.csv' #created the filename 
+    dataframe.to_csv(os.path.join('Downloads',filename)) #conversion of the data frame to csv file 
+    return render_template('download.html',filename=filename) #redirecting to the download.html page and passes filename to the html page
 
-    
+#the download.html page has redirected here but the path has filename in it so use the <> to get the dynamic file name     
 @bp.route('/download/<path:filename>',methods=['GET'])
 def download(filename):
     print(filename)
     #return f"{filename}"
-    df=pd.read_csv(f'Downloads/{filename}')
-    return df.to_html()
-    #return send_from_directory('Downloads',filename)
-    # return send_from_directory('Downloads',filename)
-    # return send_from_directory('Downloads',filename,download_name='results.csv')
-    #sreturn send_from_directory('Downloads',filename,download_name=f'{uuid.uuid4()}.csv')
+    df=pd.read_csv(f'Downloads/{filename}')#reading the csv file from the downloads
+    return df.to_html() #returning the csv file content in html page as table 
+
+#unable to create a download link for the file 
+#return send_from_directory('Downloads',filename)
+# return send_from_directory('Downloads',filename)
+# return send_from_directory('Downloads',filename,download_name='results.csv')
+# return send_from_directory('Downloads',filename,download_name=f'{uuid.uuid4()}.csv')
